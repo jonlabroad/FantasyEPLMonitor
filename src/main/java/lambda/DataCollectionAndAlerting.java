dcd3f5e39f4045a9834dbc6793dfc65a8358137a
Jonathan LaBroad <jon.labroad@gmail.com>
2017-10-29 10:55:26 -0400

    Disable pregame runner

--- src/main/java/lambda/DataCollectionAndAlerting.java
+++ src/main/java/lambda/DataCollectionAndAlerting.java
@@ -9,8 +9,8 @@ import java.util.Map;
 
 public class DataCollectionAndAlerting implements RequestHandler<Map<String, Object>, Void>{
     public Void handleRequest(Map<String, Object> params, Context context) {
-        PregameRunner pregame = new PregameRunner();
-        pregame.run();
+        //PregameRunner pregame = new PregameRunner();
+        //pregame.run();
 
         GamedayRunner gameday = new GamedayRunner();
         gameday.run();
