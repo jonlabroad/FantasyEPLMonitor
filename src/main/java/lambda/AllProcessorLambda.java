c1723d262e586861aae163e8ac107794b54c2b37
Jonathan LaBroad <jon.labroad@gmail.com>
2017-12-28 11:16:12 -0500

    main all processor

--- src/main/java/lambda/AllProcessorLambda.java
+++ src/main/java/lambda/AllProcessorLambda.java
@@ -1,10 +1,19 @@
 package lambda;
 
+import client.EPLClient;
+import client.EPLClientFactory;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import config.CloudAppConfigProvider;
 import config.GlobalConfig;
 import config.PlayerProcessorConfig;
+import data.eplapi.BootstrapStatic;
+import data.eplapi.Event;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import processor.PlayerProcessor;
 import processor.TeamProcessor;
 
@@ -13,7 +22,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AllProcessorLambda implements RequestHandler<Map<String, Object>, String> {
+    EPLClient _client =EPLClientFactory.createClient();
+
     public String handleRequest(Map<String, Object> params, Context context) {
+        if (!isTimeToPoll()) {
+            System.out.println("It's not time yet! Quiting...");
+            return "No polling to do";
+        }
+
+        Event currentEvent = getCurrentEvent();
+        if (currentEvent.id != GlobalConfig.CloudAppConfig.CurrentGameWeek) {
+            GlobalConfig.CloudAppConfig.CurrentGameWeek = currentEvent.id;
+            new CloudAppConfigProvider().write(GlobalConfig.CloudAppConfig);
+        }
+
         ILambdaInvoker invoker = new LocalAwsLambdaInvoker();
         PlayerProcessorConfig.getInstance().refresh(); // There appears to be caching going on (objs not unloaded from mem)
         try {
@@ -23,5 +45,60 @@ public class AllProcessorLambda implements RequestHandler<Map<String, Object>, S
             e.printStackTrace();
         }
         return null;
+    }
+
+    private boolean isTimeToPoll() {
+        if (GlobalConfig.TestMode) {
+            return true;
+        }
+
+        DateTime currentTime = DateTime.now();
+        Event event = getCurrentEvent();
+        DateTime eventStart = getEventStartTime(event);
+        System.out.format("Start date: %s\n", formatDate(eventStart));
+        System.out.format("Current date: %s\n", formatDate(currentTime));
+        System.out.format("Finished: %b\n", event.finished);
+        System.out.format("Data checked: %b\n", event.data_checked);
+
+        if (currentTime.isAfter(eventStart) && (!event.finished || !event.data_checked)) {
+            return true;
+        }
+        return false;
+    }
+
+    private Event getCurrentEvent() {
+        BootstrapStatic boot = _client.getBootstrapStatic();
+        int currentEvent = boot.currentEvent;
+        for (Event event : boot.events) {
+            if (event.id == currentEvent) {
+                return event;
+            }
+        }
+        return null;
+    }
+
+    private String formatDate(DateTime date) {
+        return date.toString(getDateFormatter());
+    }
+
+    private DateTime getEventStartTime(Event event) {
+        DateTimeFormatter fmt = getDateFormatter();
+        return fmt.parseDateTime(event.deadline_time);
+    }
+
+    private DateTimeFormatter getDateFormatter() {
+        return DateTimeFormat.forPattern("YYYY-MM-dd'T'HH:mm:ss'Z'").withZone(DateTimeZone.forID("Europe/London"));
+    }
+
+    private void setSequenceId() {
+        PlayerProcessorConfig config = PlayerProcessorConfig.getInstance();
+        config = config.refresh();
+        config.recorderSequence++;
+        config.write();
+    }
+
+    public static void main(String[] args) {
+        Map<String, Object> params = new HashMap<>();
+        new AllProcessorLambda().handleRequest(params, null);
     }
 }
